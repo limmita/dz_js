19d87d050ab567a7089b3167372082767a3355b6
limmita <akkermanmen@mail.ru>
2017-07-25 11:23:17 +0300

    на прверку

--- dz_6/script.js
+++ dz_6/script.js
@@ -1,5 +1,6 @@
-// С регистрами наморочился и ничего не вышло. Покажите пожайлуста на уроке как перевести строку в нижний регистр и первая буква предложения заглавная.
 var text = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+
+text = text.toLowerCase();
 var array = text.split('. ');
 var reverse = [];
 var wordArray = [];
@@ -7,12 +8,16 @@ var wordArray = [];
 for (var i = 0; i < array.length; i++) {
 	wordArray[i] = array[i].split(' ');
 	reverse[i] = sortBubble(wordArray[i]);
+	reverse[i] = capitaliseFirstLetter(reverse[i]);
 
 }
 
 reverse = reverse.join('. ') + '.';
 console.log(reverse);
 
+function capitaliseFirstLetter(string){
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
 
 function sortBubble(data) {
 	var tmp;
