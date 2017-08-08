04746976f12862b6d2d183ff545aaf7b9f8a6126
limmita <akkermanmen@mail.ru>
2017-08-08 16:32:22 +0300

    на проверку

---
+++ dz_7/script.js
@@ -0,0 +1,63 @@
+var year = "January,February,March,April,May,June,July,August,September,October,November,December";
+
+
+var winter = {
+	December : {
+		number : 12,
+		days : 31
+	},
+	January : {
+		number : 1,
+		days : 31
+	},
+	February : {
+		number : 2,
+		days : 28
+	}
+};
+
+var spring = {
+	March : {
+		number : 3,
+		days : 31
+	},
+	April : {
+		number : 4,
+		days : 30
+	},
+	May : {
+		number : 5,
+		days : 31
+	}
+};
+
+var summer = {
+	June : {
+		number : 6,
+		days : 30
+	},
+	July : {
+		number : 7,
+		days : 31
+	},
+	August : {
+		number : 8,
+		days : 31
+	}
+};
+
+var autumn = {
+	September : {
+		number : 9,
+		days : 30
+	},
+	October : {
+		number : 10,
+		days : 31
+	},
+	November : {
+		number : 11,
+		days : 30
+	}
+};
+
